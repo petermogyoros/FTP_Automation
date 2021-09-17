9dd31f58e1cf67be6a8986a5ec00bf9e435dda53
petermogyoros <petermogyoros@yahoo.co.uk>
2021-09-17 18:20:20 +0100

    Connection established but Traceback for No Such File..

--- ftp_sctipt_LN_HMI.py
+++ ftp_sctipt_LN_HMI.py
@@ -2,17 +2,42 @@ from ftplib import FTP
 import fnmatch, sys, os, csv
 import pandas as pd
 
-
 # FTP Section
 ftp = FTP('192.168.3.99')  # connect to remote host
 ftp.login(user='Admin', passwd='0000')
 
 def get_data() -> str:
-    ftp.cwd('/Project1')
-    ftp.retrlines('LIST')
+    ftp.cwd('/Project1/LOG00001')
+    root_dir = ftp.nlst()
+    local_dir = os.chdir('C:\\Users\petermogyoros\Documents')
+    pattern = '*.csv'  # choose search patern
+    for l in root_dir:
+        # only find files with the .csv extention
+        if fnmatch.fnmatch(l, pattern):
+            local_dir = 'C:\\Users\petermogyoros\Documents\FTP_LN_HMI'
 
-get_data()
+            # Create directory (local_dir) if it doesn't already exist
+            if os.path.exists(local_dir):
+                pass
+            else:
+                os.mkdir(local_dir)
 
+            print(ftp.getwelcome())  # FTP Server returns a welcome message
+
+            return(l)
+
+
+
+data = get_data()
+
+with open(data, newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar=',')
+    for row in spamreader:
+        print(', '.join(row))
+#pd.DataFrame([x.split(';') for x in data.split('\n')])
+#print(df)
+
+# ftp.delete(data)
 """
 
 # 250g Dawn Red P16 prog:025
